e32393117d183deae823fd86d1254d74b11fabee
zhuyc1212 <117319582+zhuyc1212@users.noreply.github.com>
2024-03-31 03:03:57 -0400

    updated new model to contain errors for categorical variables, and updated logistic model to include pipeline and refining of variables

--- app_csv.py
+++ app_csv.py
@@ -13,12 +13,107 @@ app = FastAPI()
 model = joblib.load("/Users/zhuyuchen/Desktop/CMU MSBA/Mini 4/Machine Learning For Business Applications/logistic_regression_model.pkl")
 columns = pd.read_csv("/Users/zhuyuchen/Desktop/CMU MSBA/Mini 4/Machine Learning For Business Applications/hotel cancellation/data_columns.csv")
 
+# Define classes for categorical variables
+class HotelType(str, Enum):
+    CityHotel = 'City Hotel'
+    ResortHotel = 'Resort Hotel'
+
+class Month(Enum):
+    January = 'January'
+    February = 'February'
+    March = 'March'
+    April = 'April'
+    May = 'May'
+    June = 'June'
+    July = 'July'
+    August = 'August'
+    September = 'September'
+    October = 'October'
+    November = 'November'
+    December = 'December'
+
+class WeekOfYear(Enum):
+    pass
+
+class Meal(Enum):
+    BB = 'BB'
+    FB = 'FB'
+    HB = 'HB'
+    SC = 'SC'
+    NA = 'Undefined'
+
+class Country(Enum):
+    pass
+
+class MarketSegment(Enum):
+    pass
+
+class Distribution(Enum):
+    pass
+
+class RoomType(Enum):
+    pass
+
+class DepositType(Enum):
+    NoDeposit = 'No Deposit'
+    Refundable = 'Refundable'
+    NonRefundable = "Non Refund"
+
+class CustomerType(Enum):
+    Transient = 'Transient'
+    Contract = 'Contract'
+    TransientParty = 'Transient-Party'
+    Group = 'Group'
+
+for i in range(1, 54):
+    setattr(WeekOfYear, f"Week{i}", i)
+
+unique_countries = ['PRT', 'GBR', 'USA', 'ESP', 'IRL', 'FRA', 'ROU', 'NOR', 'OMN',
+       'ARG', 'POL', 'DEU', 'BEL', 'CHE', 'CN', 'GRC', 'ITA', 'NLD',
+       'DNK', 'RUS', 'SWE', 'AUS', 'EST', 'CZE', 'BRA', 'FIN', 'MOZ',
+       'BWA', 'LUX', 'SVN', 'ALB', 'IND', 'CHN', 'MEX', 'MAR', 'UKR',
+       'SMR', 'LVA', 'PRI', 'SRB', 'CHL', 'AUT', 'BLR', 'LTU', 'TUR',
+       'ZAF', 'AGO', 'ISR', 'CYM', 'ZMB', 'CPV', 'ZWE', 'DZA', 'KOR',
+       'CRI', 'HUN', 'ARE', 'TUN', 'JAM', 'HRV', 'HKG', 'IRN', 'GEO',
+       'AND', 'GIB', 'URY', 'JEY', 'CAF', 'CYP', 'COL', 'GGY', 'KWT',
+       'NGA', 'MDV', 'VEN', 'SVK', 'FJI', 'KAZ', 'PAK', 'IDN', 'LBN',
+       'PHL', 'SEN', 'SYC', 'AZE', 'BHR', 'NZL', 'THA', 'DOM', 'MKD',
+       'MYS', 'ARM', 'JPN', 'LKA', 'CUB', 'CMR', 'BIH', 'MUS', 'COM',
+       'SUR', 'UGA', 'BGR', 'CIV', 'JOR', 'SYR', 'SGP', 'BDI', 'SAU',
+       'VNM', 'PLW', 'QAT', 'EGY', 'PER', 'MLT', 'MWI', 'ECU', 'MDG',
+       'ISL', 'UZB', 'NPL', 'BHS', 'MAC', 'TGO', 'TWN', 'DJI', 'STP',
+       'KNA', 'ETH', 'IRQ', 'HND', 'RWA', 'KHM', 'MCO', 'BGD', 'IMN',
+       'TJK', 'NIC', 'BEN', 'VGB', 'TZA', 'GAB', 'GHA', 'TMP', 'GLP',
+       'KEN', 'LIE', 'GNB', 'MNE', 'UMI', 'MYT', 'FRO', 'MMR', 'PAN',
+       'BFA', 'LBY', 'MLI', 'NAM', 'BOL', 'PRY', 'BRB', 'ABW', 'AIA',
+       'SLV', 'DMA', 'PYF', 'GUY', 'LCA', 'ATA', 'GTM', 'ASM', 'MRT',
+       'NCL', 'KIR', 'SDN', 'ATF', 'SLE', 'LAO']
+
+for country_code in unique_countries:
+    setattr(Country, country_code, country_code)
+
+market_segment = ['Direct', 'Corporate', 'Online TA', 'Offline TA/TO', 
+                  'Complementary', 'Groups', 'Undefined', 'Aviation']
+
+for segments in market_segment:
+    setattr(MarketSegment, segments, segments)
+
+distribution_channels = ['Direct', 'Corporate', 'TA/TO', 'Undefined', 'GDS']
+
+for channels in distribution_channels:
+    setattr(Distribution, channels, channels)
+
+room_types = ['C', 'A', 'D', 'E', 'G', 'F', 'H', 'L', 'P', 'B']
+
+for types in room_types:
+    setattr(RoomType, types, types)
+
 # Define request body model
 class InputFeatures(BaseModel):
-    hotel: str
+    hotel: HotelType
     lead_time: int
     arrival_date_year: int
-    arrival_date_month: str
+    arrival_date_month: Month
     arrival_date_week_number: int
     arrival_date_day_of_month: int
     stays_in_weekend_nights: int
@@ -26,19 +121,19 @@ class InputFeatures(BaseModel):
     adults: int
     children: int
     babies: int
-    meal: str
-    country: str
-    market_segment: str
-    distribution_channel: str
+    meal: Meal
+    country: Country
+    market_segment: MarketSegment
+    distribution_channel: Distribution
     is_repeated_guest: int
     previous_cancellations: int
     previous_bookings_not_canceled: int
-    reserved_room_type: str
-    assigned_room_type: str
+    reserved_room_type: RoomType
+    assigned_room_type: RoomType
     booking_changes: int
-    deposit_type: str
+    deposit_type: DepositType
     days_in_waiting_list: int
-    customer_type: str
+    customer_type: CustomerType
     adr: float
     required_car_parking_spaces: int
     total_of_special_requests: int
@@ -106,9 +201,10 @@ async def predict_from_csv(file: UploadFile = File(...)):
     
     df_cleaned = df[features]
     # Identify categorical columns
-    categorical_columns = ['hotel', 'arrival_date_month', 'meal', 'arrival_date_year', 
-                       'arrival_date_week_number', 'arrival_date_day_of_month', 'country', 'market_segment', 
-                       'distribution_channel', 'reserved_room_type', 'assigned_room_type',  'deposit_type', 'customer_type']
+    categorical_columns = ['hotel', 'arrival_date_month', 'meal', 
+                    'arrival_date_week_number', 'country', 'market_segment', 
+                    'distribution_channel', 'reserved_room_type', 'assigned_room_type',  
+                    'deposit_type', 'customer_type']
 
     # Create dummy variables for the categorical column
     dummy_variables = pd.get_dummies(df_cleaned[categorical_columns], drop_first=True)
